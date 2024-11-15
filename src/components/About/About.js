06ef896a9f6d16ee9f0c45e1317ac0028eef6e2a
Toto <civita.anthony@gmail.com>
2024-11-15 18:11:40 +0100

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { ReactComponent as AboutSvg } from '../../assets/About.svg';
 import styles from './About.module.css';
 
 function About() {
   return (
-    <div>
-      <div className={styles.cadre1}></div>
-      <div className={styles.cadre2}></div>
+    <div className={styles.pageContent}>
+      <div className={styles.cadre1}>
+        <div className={styles.svgContainer}>
+         <AboutSvg width="75%" />
+       </div>
+      </div>
+      <div id="work" className={styles.cadre2}></div>
     </div>
   );
 }
